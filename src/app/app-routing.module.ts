8c2ab260283f6e6aab075dac43e57542faa4f6e8
Christina Giannouli <xristitina@gmail.com>
2021-03-07 19:52:56 +0200

    Create a Users component and add it to the app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
 
-const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'users', pathMatch: 'full' },
+    {
+        path: 'users',
+        component: UsersComponent,
+    },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UsersComponent } from './users.component';
 
 @NgModule({
-    declarations: [],
+    declarations: [UsersComponent],
     imports: [CommonModule],
-    exports: [],
+    exports: [UsersComponent],
 })
 export class UsersModule {}
