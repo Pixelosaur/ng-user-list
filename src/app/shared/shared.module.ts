ef38d92dd6059754dd5afa24c0271447a6b6e9b4
Christina Giannouli <xristitina@gmail.com>
2021-03-09 14:23:09 +0200

    Create a LoaderComponent

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // Components
 import { PageTitleComponent } from './components/page-title/page-title.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
-    declarations: [PageTitleComponent],
+    declarations: [PageTitleComponent, LoaderComponent],
     imports: [CommonModule],
-    exports: [PageTitleComponent, CommonModule],
+    exports: [PageTitleComponent, CommonModule, LoaderComponent],
 })
 export class SharedModule {}
