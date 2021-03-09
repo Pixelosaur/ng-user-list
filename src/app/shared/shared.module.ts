2ed8420f50ef1fdc3362dbd466d894ccabe06c29
Christina Giannouli <xristitina@gmail.com>
2021-03-09 13:16:13 +0200

    Create a shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+// Core
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [],
+    imports: [CommonModule],
+    exports: [CommonModule],
+})
+export class SharedModule {}
--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     declarations: [UsersComponent],
-    imports: [CommonModule, HttpClientModule],
+    imports: [CommonModule, HttpClientModule, SharedModule],
     exports: [UsersComponent],
 })
 export class UsersModule {}
