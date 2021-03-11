a5ab27af1db29fbe26f57b4aed3afcf137ff8239
Christina Giannouli <xristitina@gmail.com>
2021-03-11 21:59:36 +0200

    Change detection strategy to be OnPush on Alert component

--- src/app/shared/components/alert/alert.component.ts
+++ src/app/shared/components/alert/alert.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Alert } from './alert.interface';
 
 @Component({
     selector: 'app-alert',
     templateUrl: './alert.component.html',
     styleUrls: ['./alert.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AlertComponent {
     @Input() alert!: Alert;
