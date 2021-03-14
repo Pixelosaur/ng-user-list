71c581ba542594f3b442f2398295c661b9276bb6
Christina Giannouli <xristitina@gmail.com>
2021-03-14 21:49:35 +0200

    Create a theme constant

---
+++ src/app/shared/theme.ts
@@ -0,0 +1,34 @@
+import { AppThemes } from './interfaces/theme.interface';
+
+export const THEMES: AppThemes = {
+    'purple-rose': {
+        'color-main': '#5C76E7',
+        'color-accent': '#FC656C',
+        'color-gradient': 'linear-gradient(62deg, #5C76E7 0%, #FC656C 100%)',
+    },
+    'rose-yellow': {
+        'color-main': '#FC656C',
+        'color-accent': '#FADB5F',
+        'color-gradient': 'linear-gradient(62deg, #FC656C 0%, #FADB5F 100%)',
+    },
+    'yellow-purple': {
+        'color-main': '#FADB5F',
+        'color-accent': '#60CC96',
+        'color-gradient': 'linear-gradient(62deg, #FADB5F 0%, #60CC96 100%)',
+    },
+    'aqua-blue': {
+        'color-main': '#60CC96',
+        'color-accent': '#34C1FD',
+        'color-gradient': 'linear-gradient(62deg, #60CC96 0%, #34C1FD 100%)',
+    },
+    'blue-purple': {
+        'color-main': '#34C1FD',
+        'color-accent': '#5C76E7',
+        'color-gradient': 'linear-gradient(62deg, #34C1FD 0%, #5C76E7 100%)',
+    },
+    'yellow-blue': {
+        'color-main': '#FADB5F',
+        'color-accent': '#34C1FD',
+        'color-gradient': 'linear-gradient(62deg, #FADB5F 0%, #34C1FD 100%)',
+    },
+};
