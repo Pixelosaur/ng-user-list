e7ee6b5682fd560c2c4da5869a73e37759da43fb
Christina Giannouli <xristitina@gmail.com>
2021-03-14 21:51:08 +0200

    Reorganise app.module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { UsersModule } from './users/users.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
     declarations: [AppComponent],
